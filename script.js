0303466254d0636194d370646f05ab024d04af4f
Georgy Extra <georgyextra@gmail.com>
2021-08-04 13:20:33 -0400

    added crossing out option

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ const input = document.getElementById("input");
 const button = document.getElementById("button");
 const list = document.querySelector("ul");
 const cleanerButton = document.getElementById("clear");
+
 const removeParent = (event) => event.target.parentElement.remove();
+
+const checked = (event) => event.target.classList.toggle("done");
 
 const createElement = () => {
         const li = document.createElement("li");
@@ -11,6 +14,9 @@ const createElement = () => {
         destructor.textContent="delete";
         destructor.addEventListener("click", removeParent)
         li.appendChild(destructor);
+        li.classList.add('done');
+        li.classList.toggle('done');
+        li.addEventListener('click', checked)
         list.appendChild(li);
         input.value = "";
 }
